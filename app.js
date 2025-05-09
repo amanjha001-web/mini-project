629cca76b529d50a31cf4a31d70d633f481d6a61
amanjha001 <amankumarjha096@gmail.com>
2025-05-09 15:41:39 +0530

    make responsive

--- app.js
+++ app.js
@@ -6,6 +6,10 @@
     let winMsg=document.querySelector(".win");
     let newGameBtn=document.querySelector("#startNew");
     let winMatch=document.querySelector("#draw");
+    let h1=document.querySelector("h1");
+    let btnCnt=document.querySelector(".btn");
+    let con=document.querySelector(".container");
+    
     const winPattern=[
         [0,1,2],
         [0,3,6],
@@ -102,3 +106,88 @@
       star.style.animationDelay = `${Math.random() * 3}s`;
       document.body.appendChild(star);
     }
+
+    
+
+    // Make responsive
+    function checkScreenSize(){
+        if(window.innerWidth<312){
+            h1.style="font-size:37px; line-height: 60px;";
+            for(let btn of acBtn){
+                btn.style="min-height:10vh; font-size:25px;";
+            }
+            resetBtn.style="width:70px; height:27px; font-size:12px";
+            btnCnt.style="margin:50px 0px; ";
+            winMsg.style="min-width:150px;";
+            winMatch.style="font-size:25px;";
+            con.style="height:386px;";
+            
+        }
+        else if(window.innerWidth<412){
+            h1.style="font-size:42px; line-height: 55px;";
+            for(let btn of acBtn){
+                btn.style="min-height:12vh; font-size:29px;";
+            }
+            resetBtn.style="width:75px; height:30px; font-size:15px";
+            btnCnt.style="margin:43px 0px;";
+            winMsg.style="max-width:180px;";
+            winMatch.style="font-size:29px;";
+            
+        }
+        else if(window.innerWidth<512){
+            h1.style="font-size:47px; line-height: 60px;";
+            for(let btn of acBtn){
+                btn.style="min-height:14vh; font-size:32px;";
+            }
+            resetBtn.style="width:80px; height:32px; font-size:18px";
+            btnCnt.style="margin:36px 0px;";
+            winMsg.style="max-width:210px;";
+            winMatch.style="font-size:32px;";
+            
+        }
+        else if(window.innerWidth<612){
+            h1.style="font-size:55px; line-height: 65px;" ;
+            for(let btn of acBtn){
+                btn.style="min-height:16vh; font-size:35px;";
+            }
+            resetBtn.style="width:95px; height:34px; font-size:21px";
+            btnCnt.style="margin:29px 0px;";
+            winMsg.style="max-width:240px;";
+            winMatch.style="font-size:36px;";
+            
+        }
+        else if(window.innerWidth<712){
+            h1.style="font-size:62px; line-height: 70px;";
+            for(let btn of acBtn){
+                btn.style="min-height:18vh; font-size:40px;";
+            }
+            resetBtn.style="width:110px; height:36px; font-size:24px";
+            btnCnt.style="margin:22px 0px;";
+            winMsg.style="max-width:260px;";
+            winMatch.style="font-size:40px;";
+            
+        }
+        else if(window.innerWidth<850){
+            h1.style="font-size:67px; line-height: 77px;";
+            for(let btn of acBtn){
+                btn.style="min-height:20vh; font-size:45px;";
+            }
+            resetBtn.style="width:125px; height:38px; font-size:27px";
+            btnCnt.style="margin:15px 0px;";
+            winMsg.style="max-width:280px;";
+            winMatch.style="font-size:45px;";
+            
+        }else{
+            h1.style="font-size:72px; line-height: 87px;";
+            for(let btn of acBtn){
+                btn.style="min-height:22vh; font-size:50px;";
+            }
+            resetBtn.style="width:140px; height:40px; font-size:30px";
+            btnCnt.style="margin:8px 0px;";
+            winMsg.style="max-width:300px;";
+            winMatch.style="font-size:50px;";
+            
+        }
+    }
+    window.addEventListener("resize",checkScreenSize);
+    
